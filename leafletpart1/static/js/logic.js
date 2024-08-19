3c5579a0cbb95a0781c840cd122529fe8f9da84a
vidyavelusamy <vidyavelusamy@gmail.com>
2024-08-19 17:34:03 -0400

    leaflet part1 and 2

---
+++ leafletpart1/static/js/logic.js
@@ -0,0 +1,82 @@
+let myMap = L.map("map", {
+  center: [37.7749, -122.4194],
+  zoom: 6,
+});
+function markerSize(magnitude) {
+  return  magnitude * 4;
+}
+// Create the createMap function.
+
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+legend = L.control({ position: 'bottomright' })
+
+
+legend.onAdd = function (map) {
+  var div = L.DomUtil.create('div', 'info legend')
+  var grades = [0, 10, 30, 50, 70, 100];
+  var labels = [];
+  var lab_limits =['-10-10','10-30','30-50','50-70','70-90','90+']
+ div.innerHTML +=
+      '<div class="legend">'
+ 
+    for (var i = 0; i < grades.length; i++) {
+
+      div.innerHTML +=
+     '<div class="legend-item"><div class="legend-color" style="background:' + getColor(grades[i] + 1) + '"></div>' +
+      lab_limits[i] + '</div>';
+}
+div.innerHTML +='</div>  ' 
+ 
+  return div
+}
+legend.addTo(myMap)
+
+      // Function to determine the color based on depth.
+      function getColor(depth) {
+        if (depth> 100) return "#FF4500";
+        else if (depth >70) return "#FFA500";
+        else if (depth >50) return "#FFFF00";
+        else if (depth >30) return "#ADFF2F";
+        else if (depth >10) return "#7FFF00";
+        else return "#39FF14";
+      }
+// Create the createMarkers function.
+function createMarkers() {
+  // Define the URL for the GeoJSON data.
+  let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+  // Fetch the GeoJSON data from the USGS website.
+  d3.json(link).then(function (data) {
+      console.log(data.features);
+      
+
+
+
+      // Function to determine the size based on magnitude.
+      function getSize(magnitude) {
+          return magnitude * 4; // 
+      }
+
+      // Create a GeoJSON layer and add it to the map.
+      let myLayer = L.geoJSON(data.features, {
+          pointToLayer: function (feature, latlng) {
+              // Create a circle marker with color and size based on earthquake properties.
+              return L.circleMarker(latlng, {
+                  radius: getSize(feature.properties.mag),
+                  fillColor: getColor(feature.geometry.coordinates[2]),
+                  color: '#000',
+                  weight: 1,
+                  opacity: 1,
+                  fillOpacity: 0.75
+              }).bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+          }
+      }).addTo(myMap)
+
+      
+  });
+}
+
+createMarkers()
